fddbb1f7ef06b623ca54e29fe6a8d7ddefcd48df
WalterRomero17 <walter.gabriel.romero@gmail.com>
2023-10-23 15:17:11 -0300

    uso ascii para los caracteres

--- Codigo a alto nivel/Main.py
+++ Codigo a alto nivel/Main.py
@@ -1,21 +1,19 @@
 #Asignacion de datos
 
-ABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 MensajeCodificado = ""
 
 #Input
-inputUsuario = input ("ingrese el corrimiento del mensaje y luego Ingrese el mensaje que desea codificar")
+inputUsuarioCor = int(input ("ingrese el corrimiento del mensaje."))
+inputUsuarioMen = input (" Ingrese el mensaje que desea codificar")
 
-inputUsuario_Lista = list(inputUsuario)
-Corrimiento = inputUsuario_Lista[0]
-Mensaje_a_decodificar = inputUsuario_Lista[1:]
 
 #Funciones
-#Recorro el input del usuario en forma de lista y el ABC, busco en indice de las letras que coinciden. creo una cadena con el corrimiento elegido
-for Letra_ABC in ABCD:
-  for Letra_Mensaje in Mensaje_a_decodificar:
-    if Letra_ABC  == Letra_Mensaje:
-      MensajeCodificado += ABCD[ABCD.index(Letra_ABC) + int(Corrimiento)]
+for letra_Mensaje in inputUsuarioMen:
+    numAscii = ord(letra_Mensaje)
+    nuevoNumAscii = numAscii + inputUsuarioCor
+    if nuevoNumAscii > 122:
+        nuevoNumAscii = nuevoNumAscii - 122 + 96
 
-print ("mensaje secreto =" + MensajeCodificado)
+    MensajeCodificado += chr( nuevoNumAscii)
+
+print ("mensaje secreto = " + MensajeCodificado)
