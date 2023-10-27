20f19961e56b2718dbbf1faeda444e4b3247286e
Damian D <55171195+D4m0lc3s@users.noreply.github.com>
2023-10-27 00:10:34 -0300

    Corrimiento ampliado
    Actualizo a una versión dinámica con corrimiento de 5 posiciones

--- Codigo a alto nivel/Main.py
+++ Codigo a alto nivel/Main.py
@@ -9,18 +9,26 @@ inputUsuario = input ("Ingrese el mensaje que desea codificar, luego ingrese el 
 
 inputUsuario = inputUsuario.split(";")
 inputUsuarioOpc = inputUsuario[-1]
-inputUsuarioCor = int(inputUsuario[-2])
-inputUsuarioMen = inputUsuario[-3]
+inputUsuarioCor = inputUsuario[-6:-1]
+inputUsuarioMen = inputUsuario[-7]
+IndiceCor = 0
 
 #Funcion Codificar
 if inputUsuarioOpc == "c":
   for letra_Mensaje in inputUsuarioMen:
+    
     numAscii = ord(letra_Mensaje)
-    nuevoNumAscii = numAscii + inputUsuarioCor
+    #obtengo el nuemro ascii de la letra con el corrimiento  
+    nuevoNumAscii = numAscii + int(inputUsuarioCor[IndiceCor])
+    #recorro los indices de los corrimientos
+    IndiceCor = IndiceCor + 1
+    if IndiceCor > 4:
+        IndiceCor = 0
+    #en caso de que el numero ascii sea mayor a z, lo corro para que vuelva a estar dentro del rango
     if nuevoNumAscii > 122:
         nuevoNumAscii = nuevoNumAscii - 122 + 96
-
     MensajeCodificado += chr(nuevoNumAscii)
+  
   print ("mensaje secreto = " + MensajeCodificado)
   
 #Funcion decodificar
@@ -28,11 +36,16 @@ elif inputUsuarioOpc == "d" :
 
   for letra_Mensaje in inputUsuarioMen:
     numAscii = ord(letra_Mensaje) 
-    nuevoNumAscii = numAscii + (inputUsuarioCor * -1)
+    nuevoNumAscii = numAscii + (int(inputUsuarioCor[IndiceCor]) * -1)
+    IndiceCor = IndiceCor + 1
+    if IndiceCor > 4:
+        IndiceCor = 0
+    #en caso de que el numero ascii sea mayor a a, lo corro para que vuelva a estar dentro del rango
     if nuevoNumAscii < 97:
        nuevoNumAscii = nuevoNumAscii - 96 + 122
     MensajeDecodificado += chr(nuevoNumAscii)
   print ("mensaje decodificado = " + MensajeDecodificado)
+  
 #Input incorrecto
 else:
   print ("Ingrese una opcion valida")
