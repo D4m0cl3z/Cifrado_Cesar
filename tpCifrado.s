89223da26564ee45bfce6e4334078c6ec0abf62d
WalterRomero17 <walter.gabriel.romero@gmail.com>
2023-11-24 07:43:28 -0300

    version 24/11

--- tpCifrado.s
+++ tpCifrado.s
@@ -1,6 +1,11 @@
 .data
 
-pedidoDeMensaje: .asciz "Ingrese mensaje, valores de las claves y acción: "
+pedidoDeMensaje: .asciz "Ingrese mensaje, valores de las claves y acciÃ³n: "
+mensaje_paridad_no_correcta: .asciz "La paridad es Incorrecta\n"
+mensaje_procesar_inic: .asciz "Se procesaron "
+mensaje_procesar_fin: .asciz " letras\n"
+primer_char: .byte 0
+segundo_char: .byte 0
 inputUsuario: .space 128
 mensajeExtraido: .space 128
 claveExtraida: .space 128
@@ -91,7 +96,7 @@ codificar:
     push { r6, lr }
     mov r3, #0 @corrimiento numero
     mov r9, #0
-    mov r10, r2 @Guardo posición de memoria inicial de corrimiento
+    mov r10, r2 @Guardo posiciÃ³n de memoria inicial de corrimiento
     mov r11, #0 @indice lenclave
 
 recorrer_codificador:
@@ -99,7 +104,7 @@ recorrer_codificador:
     cmp r11, r8 @ Comparo indice de corrimiento con lenclave
     bne noreinicio_ubicaciondeclave_codificador
 
-    mov r2, r10 @ reinicio unicación de clave
+    mov r2, r10 @ reinicio unicaciÃ³n de clave
     mov r11, #0
 
 noreinicio_ubicaciondeclave_codificador:
@@ -150,7 +155,7 @@ decodificar:
 
     mov r3, #0 @corrimiento numero
     mov r9, #0
-    mov r10, r5 @Guardo posición de memoria inicial de corrimiento
+    mov r10, r5 @Guardo posiciÃ³n de memoria inicial de corrimiento
     mov r11, #0 @indice lenclave
 
 
@@ -159,13 +164,17 @@ recorrer_decodificador:
     cmp r11, r8 @ Comparo indice de corrimiento con lenclave
     bne noreinicio_ubicaciondeclave_decodificador
 
-    mov r5, r10 @ reinicio unicación de clave
+    mov r5, r10 @ reinicio unicaciÃ³n de clave
     mov r11, #0
 
 noreinicio_ubicaciondeclave_decodificador:
     ldrb r3, [r5] @ Traigo corrimiento
     ldrb r6, [r0] @ Cargo letra de memoria
 
+    cmp r6, #'0'
+    beq salir_decodificar
+    cmp r6, #'1'
+    beq salir_decodificar
     cmp r6, #0 @ comparo ultimo caracter con null
     beq salir_decodificar
 
@@ -174,7 +183,8 @@ noreinicio_ubicaciondeclave_decodificador:
 
     sub r6, r3 @ resto corrimiento al mensaje codificado
 
-    cmp r6, #97 @ comparo con la letra mas chica posible en ascii    bge corrimiento_singiro_decodificador
+    cmp r6, #97 @ comparo con la letra mas chica posible en ascii
+    bge corrimiento_singiro_decodificador
 
     add r6, r6, #26 @ sumo 26 para simular un giro completo
 
@@ -268,19 +278,94 @@ salirEsPar:
     bx lr
     .fnend
 
+comprobar_paridad:
+    .fnstart
+    push { lr }
+    mov r2, r9
+ciclo_paridad:
+    sub r2, #2
+    cmp r2, #1
+    bgt ciclo_paridad
+    beq paridad_impar
+paridad_par:
+    mov r3, #0x30
+    bal comparar_paridad
+paridad_impar:
+    mov r3, #0x31
+comparar_paridad:
+    cmp r1,r3
+    beq paridad_correcta
+paridad_incorrecta:
+    mov r4, #0
+    bal salir_paridad
+paridad_correcta:
+    mov r4, #1
+salir_paridad:
+    pop { lr }
+    bx lr
+    .fnend
+
+calcular_segundo_caracter:
+    .fnstart
+    push { lr }
+    mov r1, r9
+ciclo_char:
+    cmp r1, #10
+    blt conver_en_char
+    sub r1, #10
+    bal ciclo_char
+conver_en_char:
+    mov r2, r1
+    add r2, #0x30
+    ldr r3 , =segundo_char
+    strb r2, [r3]
+    pop { lr }
+    bx lr
+    .fnend
+
+calcular_primer_caracter:
+    .fnstart
+    push { lr }
+    mov r2, r1
+    mov r3, r9
+    mov r4, #0
+    sub r3,r2
+    cmp r3, #10
+    blt conver_espacio
+ciclo_char_1:
+    cmp r3, #0
+    beq conver_en_char_1
+
+    sub r3, #10
+    add r4, #1
+    bal ciclo_char_1
+conver_espacio:
+    ldr r6, =primer_char
+    mov r7, #' '
+    strb r7, [r6]
+    bal salir_primer_char
+conver_en_char_1:
+    add r4, #0x30
+    ldr r3 , =primer_char
+    strb r4, [r3]
+salir_primer_char:
+    pop { lr }
+    bx lr
+    .fnend
+
 .global main
 
 main:
    mov r0, #1  @ Descriptor de archivo (1 para stdout)
    ldr r1, =pedidoDeMensaje
    mov r2, #49
-   mov r7, #4  @ Código de llamada al sistema para write (4)
+   mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
    swi 0       @ Llama al sistema para escribir la cadena
 
    mov r0, #0            @ Descriptor de archivo (0 para stdin)
    ldr r1, =inputUsuario
-   mov r2, #128           @ Tamaño del búfer
-   mov r7, #3            @ Código de llamada al sistema para read (3)
+   mov r2, #128           @ TamaÃ±o del bÃºfer
+   mov r7, #3            @ CÃ³digo de llamada al sistema para read (3)
    swi 0                @ Llama al sistema para leer la entrada del usuario
 
 
@@ -312,38 +397,96 @@ main:
     cmp r3, #'C'
     bleq codificar
 
+    ldr r11, =opcionExtraida
+    ldrb r12, [r11]
+    cmp r12, #'c'
+    bleq esPar
+    cmp r12, #'C'
+    bleq esPar
+
+    cmp r12, #'c'
+    beq imprimir
+    cmp r12, #'C'
+    beq imprimir
+
     cmp r3, #'d'
     bleq decodificar
     cmp r3, #'D'
     bleq decodificar
 
-    bl esPar
+    ldrb r1, [r0]
+    bl comprobar_paridad
 
 
-//    mov r0, #1  @ Descriptor de archivo (1 para stdout)
-//    ldr r1, =mensajeExtraido
-//    mov r2, #4
-//    mov r7, #4  @ Código de llamada al sistema para write (4)
-//    swi 0       @ Llama al sistema para escribir la cadena
+    mov r2 , #'\n'
+    ldr r3, =mensajeCodificado
+    strb r2, [r3, r9]
 
-//    mov r0, #1  @ Descriptor de archivo (1 para stdout)
-//    ldr r1, =claveExtraida
-//    mov r2, #4
-//    mov r7, #4  @ Código de llamada al sistema para write (4)
-//    swi 0       @ Llama al sistema para escribir la cadena
+    add r9, #1
+    cmp r4, #1
+    beq imprimir
+imprimir_error:
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
+    mov r0, #1  @ Descriptor de archivo (1 para stdout)
+    mov r2, #25
+    ldr r1, =mensaje_paridad_no_correcta
+    swi 0       @ Llama al sistema para escribir la cadena
 
-//    mov r0, #1  @ Descriptor de archivo (1 para stdout)
-//    ldr r1, =opcionExtraida
-//    mov r2, #1
-//    mov r7, #4  @ Código de llamada al sistema para write (4)
-//    swi 0       @ Llama al sistema para escribir la cadena
-
-    mov r7, #4  @ Código de llamada al sistema para write (4)
+    b salir_sistema
+imprimir:
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
     mov r0, #1  @ Descriptor de archivo (1 para stdout)
     mov r2, r9
     ldr r1, =mensajeCodificado
     swi 0       @ Llama al sistema para escribir la cadena
 
+imprimir_procesamiento:
+    
+    cmp r12, #'c'
+    beq correc_long_cod
+    cmp r12, #'C'
+    beq correc_long_cod
+
+    cmp r12, #'d'
+    beq correc_long_decod
+    cmp r12, #'D'
+    beq correc_long_decod
+
+correc_long_cod:
+    sub r9, #2
+    b calcular_caracteres_long
+correc_long_decod:
+    sub r9, #1
+calcular_caracteres_long:
+    bl calcular_segundo_caracter
+    bl calcular_primer_caracter
+
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
+    mov r0, #1  @ Descriptor de archivo (1 para stdout)
+    mov r2, #14
+    ldr r1, =mensaje_procesar_inic
+    swi 0       @ Llama al sistema para escribir la cadena
+
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
+    mov r0, #1  @ Descriptor de archivo (1 para stdout)
+    mov r2, #1
+    ldr r1, =primer_char
+    swi 0       @ Llama al sistema para escribir la cadena
+
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
+    mov r0, #1  @ Descriptor de archivo (1 para stdout)
+    mov r2, #1
+    ldr r1, =segundo_char
+    swi 0       @ Llama al sistema para escribir la cadena
+
+    mov r7, #4  @ CÃ³digo de llamada al sistema para write (4)
+    mov r0, #1  @ Descriptor de archivo (1 para stdout)
+    mov r2, #8
+    ldr r1, =mensaje_procesar_fin
+    swi 0       @ Llama al sistema para escribir la cadena
+
+
+salir_sistema:
     @ Salir del programa usando la llamada al sistema de salida
     mov r7, #1            @ Codigo de llamada al sistema para exit (1)
     swi 0                @ Llama al sistema
